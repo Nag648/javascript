7af51aa218dc6b184bdb3af9d9670b3ccbcb6a91
cheeraboina srilakshmi <kongalasrilakshmi@gmail.com>
2023-10-18 15:03:32 +0530

    While and do while

---
+++ While Loop.js
@@ -0,0 +1,22 @@
+// program to display numbers from 1 to 5
+// initialize the variable
+let i = 1, n = 5;
+
+// while loop from i = 1 to 5
+while (i <= n) {
+    console.log(i);
+    i += 1;
+}
+
+
+
+//do while//
+// program to display numbers
+let j = 1;
+const m = 5;
+
+// do...while loop from 1 to 5
+do {
+    console.log(j);
+    j++;
+} while(j <= m);
