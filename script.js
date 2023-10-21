11c110003f26ee3065db11bce56365358b5e56a4
cheeraboina srilakshmi <kongalasrilakshmi@gmail.com>
2023-10-21 16:24:21 +0530

    map,reduce

---
+++ script.js
@@ -0,0 +1,112 @@
+//map, filter and reduce//
+
+//what is map()  ?
+               
+                   //map()//
+const nums = [1,2,3,4,5];
+
+const maltiply = nums.map((num, i, arr) =>{
+       return num * 2 + i;
+});
+
+console.log(maltiply);
+                  
+                 //filter()//
+const num1 = ["raju","raghu","sri","lakshmi"];
+
+const str = num1.filter((String) =>{
+    return String > "sri";
+});
+console.log(num1);
+
+
+                     //reduce//
+const num2 = [1,2,3,4,5,];
+const sum = num2.reduce((acc, curr, i, arr) => {
+    return acc +curr;
+},0);
+
+console.log(sum);
+
+// map //
+let map = new Map([
+    [1 , 10], [2 , 20] ,
+    [3, 30],[4, 40]
+    ]);
+     
+console.log("Map1: ");
+console.log(map);
+
+
+//map - 2 //
+let map1 = new Map([ 
+	[1, 2], 
+	[2, 3], 
+	[4, 5] 
+]); 
+
+console.log("Map1"); 
+console.log(map1); 
+
+let map2 = new Map([ 
+	["firstname", "srilakshmi"], 
+	["lastname", "cheeraboina"], 
+	["website", "srilakshmi@.com"] 
+]); 
+
+console.log("Map2"); 
+console.log(map2);
+
+
+//MAp - 3 //
+const arr = [4, 5, 10, 3, 8, 6];
+let result = [];
+
+// Square function returns square of a number
+const square = function (num) {
+	return num * num;
+}
+
+for (let i = 0; i < arr.length; i++) {
+	result.push(square(arr[i]));
+}
+
+// Expected output: [16 ,25, 100, 9, 64, 36]
+console.log(result);
+
+
+
+//mapget()//
+
+let GFGMap = new Map(); 
+
+// Adding a key value pair 
+GFGMap.set(1, "India"); 
+
+// Getting the value by key 
+console.log(GFGMap.get(1)); 
+
+// Updating the existing value 
+GFGMap.set(1, "England"); 
+
+// Getting the value back 
+console.log(GFGMap.get(1)); 
+
+
+//map forEach()//
+
+// Creating a map using Map object
+let mp = new Map()
+
+// Adding values to the map
+mp.set("a", 1);
+mp.set("b", 2);
+mp.set("c", 3);
+
+// Logging map object to console
+mp.forEach((values, keys) => {
+	console.log(values, keys)
+})
+
+
+
