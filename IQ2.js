65f4a1178d6edc076d12958317be038f4f174860
cheeraboina srilakshmi <kongalasrilakshmi@gmail.com>
2023-12-21 22:42:48 +0530

    IQ

---
+++ IQ2.js
@@ -0,0 +1,15 @@
+//    length of string  1   //
+
+let text = "Hello World";
+let length = text.length;
+
+console.log(length);
+
+//     lowerCase of string  2  //
+// function fun(){
+let text2 = "Hello Worid";
+let string = text2.toLowerCase();
+
+console.log(string);
+// }
+// fun()
