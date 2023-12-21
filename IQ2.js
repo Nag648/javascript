e1ca437bb9a85806d7420b05e5c1056104167ca4
cheeraboina srilakshmi <kongalasrilakshmi@gmail.com>
2023-12-21 23:02:02 +0530

    IQ2

--- IQ2.js
+++ IQ2.js
@@ -7,9 +7,41 @@ console.log(length);
 
 //     lowerCase of string  2  //
 // function fun(){
-let text2 = "Hello Worid";
+let text2 = "Hello World";
 let string = text2.toLowerCase();
 
 console.log(string);
 // }
 // fun()
+
+   //     to Upper Case()  3   //
+
+
+   let str ="hello world";
+   let string1 = str.toUpperCase();
+   console.log(string1);
+
+
+   //       subString    4    //
+
+   let str1 = "This is my example string";
+   const substr = "my";
+   console.log(str1.includes(substr));
+
+
+
+        //         charAt()     5       //
+
+
+        let str2 = "HELLO WORLD";
+        let letter = str2.charAt(4);
+
+        console.log(letter);
+
+
+         //          replace a substring       6       //
+
+
+         let originalstring = "sri lakshmi";
+         let newstring = originalstring.replace("nag");
+         console.log(newstring);
