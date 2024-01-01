5817d8448effdc919f66d09010b5e37df962d2c1
srilakshmi kongala <kongalasrilakshmi.com>
2024-01-01 11:40:00 +0530

    commit

--- script.js
+++ script.js
@@ -110,3 +110,44 @@ mp.forEach((values, keys) => {
 
 
 
+
+//null
+
+var x = null;
+console.log(typeof(x));
+
+
+
+//undefined
+var y;
+console.log(typeof(y));
+
+
+
+//   map    //
+
+const array1 = [1, 4, 9, 16];
+
+// Pass a function to map
+const mp1 = array1.map((x) => x * 2);
+
+console.log(mp1);
+
+
+
+const input = "srilakshmi";
+
+let reversed = "";
+for(let i = input.length -1; i>=0; i--){
+    reversed += input[i];
+}
+console.log(reversed);
+
+
+
+
+
+
+
+
+
